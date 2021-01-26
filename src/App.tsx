4b37513bebba25e7e9a59eb29f392ac725903e34
Ziga Lah <ziga.lah@inova.si>
2021-01-26 22:07:37 +0100

    * Eslint fixes * Added styled-components and material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-// import Intervention from './interfaces';
 import {observer} from 'mobx-react-lite';
 import StoreProvider from './StoreProvider';
 import ObservableInterventionsList from './components/ObservableInterventionsList/ObservableInterventionsList';
 import InterventionForm from './components/InterventionForm/InterventionForm';
 import InfoHeader from './components/InfoHeader/InfoHeader';
-// import {observable} from 'mobx';
-// import {useState} from 'react';
 
 
 const App = observer(() => {
